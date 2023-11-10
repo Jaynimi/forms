ad389d611e4d77efc26d95bb77363bdd40cd88c9
Jaynimi <Eyeslikejays@yahoo.com>
2023-11-10 10:53:35 +0100

    Lessons

--- react lessons/src/Greeting.jsx
+++ react lessons/src/Greeting.jsx
@@ -1,36 +1,56 @@
-function List(props) {
-	if (!props.animals) {
-		return <div>Loading...</div>;
-	}
+// function List(props) {
+// 	if (!props.animals) {
+// 		return <div>Loading...</div>;
+// 	}
 
-	if (props.animals.length === 0) {
-		return <div>There are no animals in the list!</div>;
-	}
+// 	if (props.animals.length === 0) {
+// 		return <div>There are no animals in the list!</div>;
+// 	}
 
+// 	return (
+// 		<ul>
+// 			{props.animals.map((animal) => {
+// 				return <li key={animal}>{animal}</li>;
+// 			})}
+// 		</ul>
+// 	);
+// }
+
+// function App() {
+// 	const animals = [];
+
+// 	return (
+// 		<div>
+// 			<h1>Animals: </h1>
+// 			<List animals={animals} />
+// 		</div>
+// 	);
+// }
+import { v4 as uuidv4 } from "uuid";
+
+const todos = [
+	{ task: "mow the yard", id: uuidv4() },
+	{ task: "Work on Odin Projects", id: uuidv4() },
+	{ task: "feed the cat", id: uuidv4() },
+];
+
+function TodoList() {
 	return (
 		<ul>
-			{props.animals.map((animal) => {
-				return <li key={animal}>{animal}</li>;
-			})}
+			{todos.map((todo) => (
+				// here we are using the already generated id as the key.
+				<li key={todo.id}>
+					{todo.task} {todo.id}
+				</li>
+			))}
 		</ul>
-	);
-}
-
-function App() {
-	const animals = [];
-
-	return (
-		<div>
-			<h1>Animals: </h1>
-			<List animals={animals} />
-		</div>
 	);
 }
 
 function Greeting() {
 	return (
 		<>
-			<App />
+			<TodoList />
 		</>
 	);
 }
